8f02950c9f13cd0d08b533c9f4668d070eca02ba
Adriano Santos <asrsantos@gmail.com>
2022-04-11 08:49:56 -0300

    Atualização CRUD

--- 2. Servidor/controllers/controllers.usuarios.pas
+++ 2. Servidor/controllers/controllers.usuarios.pas
@@ -65,14 +65,75 @@ begin
   end;
 end;
 
+procedure DoInsert(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  LDAO    : TADRConnDAOUsuario;
+  LConn   : IADRConnection;
+  LResult : TJSONObject;
+  LID     : Integer;
+begin
+  //Testamos se recebemos um JSON no Body
+  if Req.Body.IsEmpty then
+    raise Exception.Create('Corpo da requisição inválido. Envie um JSONArray com os dados a serem inseridos.');
+
+  LConn   := TADRConnModelFactory.GetConnectionIniFile;
+  LConn.Connect;
+
+  LDAO   := TADRConnDAOUsuario.Create(LConn);
+  try
+    LResult := LDAO.Insert(Req.Body);
+    if LResult.Count > 0 then
+      Res
+        .Send<TJSONObject>(LResult)
+        .Status(THTTPStatus.Created)
+    else
+      Res
+        .Send<TJSONObject>(LResult)
+        .Status(THTTPStatus.NoContent);
+  finally
+    LDao.Free;
+  end;
+end;
+
+procedure DoUpdate(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var
+  LDAO    : TADRConnDAOUsuario;
+  LConn   : IADRConnection;
+  LResult : TJSONObject;
+  LID     : Integer;
+begin
+  if not TryStrToInt(Req.Params['id'] , LID) then
+    raise Exception.Create('ID inválido. Envie um número inteiro.');
+
+  //Testamos se recebemos um JSON no Body
+  if Req.Body.IsEmpty then
+    raise Exception.Create('Corpo da requisição inválido. Envie um JSONArray com os dados a serem inseridos.');
+
+  LConn   := TADRConnModelFactory.GetConnectionIniFile;
+  LConn.Connect;
+  LDAO   := TADRConnDAOUsuario.Create(LConn);
+  try
+    LResult := LDAO.Update(LID, Req.Body);
+    if LResult.Count > 0 then
+      Res
+        .Send<TJSONObject>(LResult)
+        .Status(THTTPStatus.Created)
+    else
+      Res
+        .Send<TJSONObject>(LResult)
+        .Status(THTTPStatus.NoContent);
+  finally
+    LDao.Free;
+  end;
+end;
+
 procedure Registry;
 begin
   THorse.Get   ('/usuarios'     , DoList);
   THorse.Get   ('/usuarios/:id' , DoFind);
-
-//  THorse.Post  ('/usuarios', DoInsert);
-//  THorse.Put   ('/usuarios', DoUpdate);
-//  THorse.Delete('/usuarios', DoDelete);
+  THorse.Post  ('/usuarios'     , DoInsert);
+  THorse.Put   ('/usuarios/:id' , DoUpdate);
+  //THorse.Delete('/usuarios/:id' , DoDelete);
 end;
 
 
--- 2. Servidor/dao/dao.usuarios.pas
+++ 2. Servidor/dao/dao.usuarios.pas
@@ -23,12 +23,39 @@ type
     public
       function List: TJSONArray;
       function Find(AID: Integer): TJSONObject;
+      function Insert(AValue: string): TJSONObject;
+      function Update(AID: Integer; AValue: string): TJSONObject;
   end;
 
 
 implementation
 
 { TADRConnDAOUsuario }
+
+function TADRConnDAOUsuario.List: TJSONArray;
+{$Region 'SELECT'}
+const
+  LSelect =
+    'SELECT ID, USUARIO, SENHA, TIPOUSUARIO FROM USUARIOS ORDER BY ID';
+{$EndRegion}
+var
+  LDataSet : TDataSet;
+begin
+  try
+    try
+      LDataset :=
+        FQuery
+          .SQL(LSelect)
+          .OpenDataSet;
+
+      Result := LDataset.ToJSONArray;
+    except
+      Result := TJSONArray.Create;
+    end;
+  finally
+    LDataSet.Free;
+  end;
+end;
 
 function TADRConnDAOUsuario.Find(AID: Integer): TJSONObject;
 {$Region 'SELECT'}
@@ -57,29 +84,62 @@ begin
 
 end;
 
-function TADRConnDAOUsuario.List: TJSONArray;
+function TADRConnDAOUsuario.Insert(AValue: string): TJSONObject;
 {$Region 'SELECT'}
 const
   LSelect =
-    'SELECT ID, USUARIO, SENHA, TIPOUSUARIO FROM USUARIOS ORDER BY ID';
+    'SELECT ID, USUARIO, SENHA, TIPOUSUARIO FROM USUARIOS WHERE 1 = 2';
 {$EndRegion}
 var
   LDataSet : TDataSet;
 begin
   try
     try
-      LDataset :=
+      LDataSet :=
         FQuery
           .SQL(LSelect)
           .OpenDataSet;
 
-      Result := LDataset.ToJSONArray;
+      LDataSet
+        .LoadFromJSON(AValue);
+
+      Result := LDataSet.ToJSONObject;
     except
-      Result := TJSONArray.Create;
+      Result := TJSONObject.Create;
     end;
   finally
     LDataSet.Free;
   end;
+end;
+
+function TADRConnDAOUsuario.Update(AID: Integer; AValue: string): TJSONObject;
+{$Region 'SELECT'}
+const
+  LSelect =
+    'SELECT ID, USUARIO, SENHA, TIPOUSUARIO FROM USUARIOS WHERE ID = :pID';
+{$EndRegion}
+var
+  LDataSet : TDataSet;
+begin
+  try
+    try
+      LDataSet :=
+        FQuery
+          .SQL(LSelect)
+          .ParamAsInteger('pID', AID)
+          .OpenDataSet;
+
+      LDataSet
+        .MergeFromJSONObject(AValue);
+
+      Result := LDataSet.ToJSONObject;
+    except
+      Result := TJSONObject.Create;
+    end;
+  finally
+    LDataSet.Free;
+  end;
+
 end;
 
 end.
